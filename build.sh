d950d2ab5c062c4730bf163d90ed41a6fee2954b
Richard Maxwell <richard@rsmaxwell.co.uk>
2019-11-27 14:12:08 +0000

    replace gogradle by straight forward go build

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 export GOPATH=${WORKSPACE}
-cd src/github.com/rsmaxwell/players-api
 
-export DEBUG_LEVEL=30
-export DEBUG_DEFAULT_PACKAGE_LEVEL=30
-export DEBUG_DEFAULT_FUNCTION_LEVEL=30
-export DEBUG_PACKAGE_LEVEL_httphandler=30
-export DEBUG_FUNCTION_LEVEL_httphandler_CreateCourt=30
+# export DEBUG_LEVEL=30
+# export DEBUG_DEFAULT_PACKAGE_LEVEL=30
+# export DEBUG_DEFAULT_FUNCTION_LEVEL=30
+# export DEBUG_PACKAGE_LEVEL_httphandler=30
+# export DEBUG_FUNCTION_LEVEL_httphandler_CreateCourt=30
 
 export DEBUG_DUMP_DIR=${WORKSPACE}/build/dumps
 
--- publish.sh
+++ publish.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-export GOPATH=${WORKSPACE}
-cd src/github.com/rsmaxwell/players-api
-
 gradle zip publish

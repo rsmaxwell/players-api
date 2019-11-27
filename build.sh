8ae6195f4dcd5199059108a0e58954788dde78d6
Richard Maxwell <richard@rsmaxwell.co.uk>
2019-11-27 14:31:29 +0000

    replace gogradle by straight forward go build

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ export GOPATH=${WORKSPACE}
 # export DEBUG_PACKAGE_LEVEL_httphandler=30
 # export DEBUG_FUNCTION_LEVEL_httphandler_CreateCourt=30
 
-export DEBUG_DUMP_DIR=${WORKSPACE}/build/dumps
+export DEBUG_DUMP_DIR=${WORKSPACE}/src/github.com/rsmaxwell/players-api/build/dumps
 
 gradle clean generate build

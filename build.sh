7502b93076e729f0a13f004879745ac5eae2b9cf
Richard Maxwell <richard@rsmaxwell.co.uk>
2020-01-18 13:36:48 +0000

    define repoURL to gradle

--- build.sh
+++ build.sh
@@ -12,4 +12,6 @@ export PROJECT_DIR=${WORKSPACE}/src/github.com/rsmaxwell/players-api
 export PLAYERS_API_ROOTDIR=${PROJECT_DIR}/testing/root
 export DEBUG_DUMP_DIR=${PROJECT_DIR}/build/dumps
 
+export repoUrl="https://server.rsmaxwell.co.uk/archiva"
+
 gradle clean generate build
